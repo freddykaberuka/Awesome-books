3c4df2d919c8c6126e357e978218b4c9f003ca6a
freddykaberuka <freddykaberuka2009@gmail.com>
2022-04-27 00:49:07 +0200

    add linters

--- app.js
+++ app.js
@@ -2,38 +2,34 @@ const bookForm = document.querySelector('#book_form');
 const bookContainer = document.querySelector('.books');
 
 let bookList = [];
-
-
-
-// ADD BOOKS FROM LOCAL STORAGE
-bookForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const titleInput = document.getElementById('title').value;
-    const authorInput = document.getElementById('author').value;
-    const id = Math.floor(Math.random() * 1000);
-    const book={id,titleInput,authorInput}
-    bookList.push(book);
-    // console.log(bookList);
-    localStorage.setItem('books', JSON.stringify(bookList));
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    getData();
-});
-
 // GET BOOKS FROM LOCAL STORAGE
-const getData=()=>{
-    const data = localStorage.getItem('books');
-    if (data) {
-        let parsedData = JSON.parse(data);
-        bookList = parsedData;
-            bookContainer.innerHTML = parsedData.map((book) => `
+const getData = () => {
+  const data = localStorage.getItem('books');
+  if (data) {
+    const parsedData = JSON.parse(data);
+    bookList = parsedData;
+    bookContainer.innerHTML = parsedData.map((book) => `
                 <h3>${book.titleInput}</h3>
                 <p>${book.authorInput}</p>
                 <button type="button"  onclick='deleteBook(${book.id})'>Remove</button>
             <hr>
             `).join('');
-    } 
-}
+  }
+};
+// ADD BOOKS FROM LOCAL STORAGE
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleInput = document.getElementById('title').value;
+  const authorInput = document.getElementById('author').value;
+  const id = Math.floor(Math.random() * 1000);
+  const book = { id, titleInput, authorInput };
+  bookList.push(book);
+  // console.log(bookList);
+  localStorage.setItem('books', JSON.stringify(bookList));
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  getData();
+});
 const deleteBook = (id) => {
   if (id === null) return;
   const dataToFilter = bookList.filter((book) => book.id !== id);
@@ -41,7 +37,7 @@ const deleteBook = (id) => {
   getData();
 };
 
-window.addEventListener('load',()=>{
-    getData();
-    deleteBook();
+window.addEventListener('load', () => {
+  getData();
+  deleteBook();
 });
